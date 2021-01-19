c1cb597fb5ca0b3dbb575fbbb49e850de4968522
Boris Lukianov <boris.lukianov@seniordev.com>
2021-01-19 15:26:07 +0200

    is working

--- src/main/java/com/bordozer/sparkweb/WebApp.java
+++ src/main/java/com/bordozer/sparkweb/WebApp.java
@@ -4,11 +4,13 @@ import lombok.extern.slf4j.Slf4j;
 
 import static spark.Spark.afterAfter;
 import static spark.Spark.get;
+import static spark.Spark.port;
 
 @Slf4j
 public class WebApp {
 
     public static void main(String[] args) {
+        port(8800);
         afterAfter(new LoggingFilter());
         get("/", "application/json", (request, response) -> welcome());
     }
