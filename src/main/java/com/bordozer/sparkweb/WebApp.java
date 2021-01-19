ddf237080ef58dd01d552169064508032e1dd654
Boris Lukianov <boris.lukianov@seniordev.com>
2021-01-19 15:54:38 +0200

    Load properties

--- src/main/java/com/bordozer/sparkweb/WebApp.java
+++ src/main/java/com/bordozer/sparkweb/WebApp.java
@@ -1,6 +1,13 @@
 package com.bordozer.sparkweb;
 
+import com.google.common.io.Resources;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+
+import java.io.ByteArrayInputStream;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.Properties;
 
 import static spark.Spark.afterAfter;
 import static spark.Spark.get;
@@ -10,12 +17,27 @@ import static spark.Spark.port;
 public class WebApp {
 
     public static void main(String[] args) {
-        port(8800);
+        final var properties = loadConfig();
+
+        port(Integer.parseInt(properties.getProperty("app.port")));
         afterAfter(new LoggingFilter());
         get("/", "application/json", (request, response) -> welcome());
     }
 
     private static String welcome() {
         return "Ok";
+    }
+
+    @SneakyThrows
+    private static Properties loadConfig() {
+        final Properties appProps = new Properties();
+        appProps.load(new ByteArrayInputStream(readResource("config.properties").getBytes()));
+        return appProps;
+    }
+
+    @SneakyThrows
+    public static String readResource(final String name) {
+        final URL url = Resources.getResource(name);
+        return Resources.toString(url, StandardCharsets.UTF_8);
     }
 }
